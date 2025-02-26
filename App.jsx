d6a2a8c50b75caaf51646244629041beac87998d
RojanShrestha77 <151616745+RojanShrestha77@users.noreply.github.com>
2025-02-26 21:46:21 +0545

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import Home from './pages/Home';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Registration from './pages/Registration';
-import Feed from './pages/Feed';
-import CreatePost from './pages/CreatePost';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Registration from "./pages/Registration";
+import Feed from "./pages/Feed";
+import CreatePost from "./pages/CreatePost";
+import LoginSignup from "./Components/LoginSignup";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
-      {/* Render the Home component directly */}
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Register" element={<Registration />} />
-          <Route path="/Feed" element={<Feed />} />
-          <Route path="/createpost" element={<CreatePost/>} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Register" element={<Registration />} />
+        <Route path="/Feed" element={<Feed />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/LoginSignup" element={<LoginSignup />} />
+        <Route path="/profile/:username" element={<Profile />} /> {/* Use :username */}
+      </Routes>
     </div>
   );
 }
